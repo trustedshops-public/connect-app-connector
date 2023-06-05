f2d053e38dbc9ce139dee4f975eda47c6233742d
Timo Reymann <Timo.Reymann@trustedshops.de>
2023-06-05 15:59:39 +0200

    chore(tests): Make tests run parallel

--- src/tests/resources/scripts/run.sh
+++ src/tests/resources/scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pabot --testlevelsplit --processes 1 --outputdir src/tests/results -x circleci-formatted-results.xml --pythonpath src/tests/resources --pythonpath src/tests/resources/page_objects $@ src/tests/tests
+pabot --testlevelsplit --processes 2 --outputdir src/tests/results -x circleci-formatted-results.xml --pythonpath src/tests/resources --pythonpath src/tests/resources/page_objects $@ src/tests/tests
