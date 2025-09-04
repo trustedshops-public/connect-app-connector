b6ea92acc4ccaf9e6f993283f7e93fcbafbc6129
Abhishek Hassan Chandrashekar <abhishekhc1993@gmail.com>
2025-09-04 13:50:50 +0200

    Remove current test automation code

--- src/tests/resources/scripts/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pabot --testlevelsplit --processes 1 --outputdir src/tests/results -x circleci-formatted-results.xml --pythonpath src/tests/resources --pythonpath src/tests/resources/page_objects $@ src/tests/tests
