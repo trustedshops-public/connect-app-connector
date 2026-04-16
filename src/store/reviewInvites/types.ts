ccb05477d8675c994dbe6f0d0cb0311c2dcf374d
Egzon Jonuzi <egzon.jonuzi@trustedshops.com>
2026-04-16 15:24:06 +0200

    TPSCON-4235 Adapt Connector to invite teams changes

--- src/store/reviewInvites/types.ts
+++ src/store/reviewInvites/types.ts
@@ -44,7 +44,6 @@ export interface InviteSettingsByChannelType {
   createdAt?: string
   eventTypeId?: string
   id?: string
-  enabled?: boolean
   productInviteConfiguration?: InviteConfiguration
   serviceInviteConfiguration?: InviteConfiguration
 }
